1345ee14e11a1d2fa76c5b2fccff4c695668398c
Helge Holm <helge.holm@gmail.com>
2024-04-04 12:53:18 +0200

    Move car sample data to top

--- src/sampleData.js
+++ src/sampleData.js
@@ -1,21 +1,4 @@
 export const datasets = {
-  "cancer recurrence": {
-    properties: [
-      { name: "Menopause", features: ["lt40", "ge40", "premeno"] },
-      { name: "Inv-nodes", features: ["0-2", "3-5", "6-8"] },
-      { name: "Deg-malig", features: ["1", "2", "3"] },
-      { classifier: true, name: "", features: ["Recurrence"] },
-    ],
-    entries: [
-      [0, 1, 0, 0, 1, 0, 0, 0, 1, 1],
-      [1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
-      [0, 1, 0, 0, 0, 1, 0, 0, 1, 1],
-      [0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
-      [0, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-      [0, 0, 1, 1, 0, 0, 1, 0, 0, 0],
-    ],
-  },
-
   "vehicle is car": {
     properties: [
       {
@@ -31,6 +14,23 @@ export const datasets = {
       [1, 1, 1, 0, 1, 0],
       [1, 0, 1, 1, 0, 0],
       [0, 1, 1, 0, 1, 0],
+    ],
+  },
+
+  "cancer recurrence": {
+    properties: [
+      { name: "Menopause", features: ["lt40", "ge40", "premeno"] },
+      { name: "Inv-nodes", features: ["0-2", "3-5", "6-8"] },
+      { name: "Deg-malig", features: ["1", "2", "3"] },
+      { classifier: true, name: "", features: ["Recurrence"] },
+    ],
+    entries: [
+      [0, 1, 0, 0, 1, 0, 0, 0, 1, 1],
+      [1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+      [0, 1, 0, 0, 0, 1, 0, 0, 1, 1],
+      [0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
+      [0, 0, 1, 1, 0, 0, 0, 0, 1, 1],
+      [0, 0, 1, 1, 0, 0, 1, 0, 0, 0],
     ],
   }
 };
